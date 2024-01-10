cb35bf03fe3c2d37442a3b0a153f92a4e46c9bd1
2901shivam <shivamsingh201617@gmail.com>
2024-01-10 12:18:42 +0530

    one more new insertion

--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 int num = 5;
-for(int i = 1; i <= 10; i++)
+Console.WriteLine("Table of 5");
+for (int i = 1; i <= 10; i++)
 {
     Console.WriteLine(num * i);
 }
+Console.WriteLine("Table of 6");
 int num1 = 6;
 for(int i = 1; i <= 10; i++)
 {
     Console.WriteLine(num1*i);
+}
+Console.WriteLine("Table of 7");
+int num2 = 7;
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(num2 * i);
 }
